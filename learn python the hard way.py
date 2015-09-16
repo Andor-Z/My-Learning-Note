8e12d221d4d2c740d361871b53156230634b3f50
Andor-Z <zyfsta@outlook.com>
2015-09-17 00:06:21 +0800

    add

--- learn python the hard way.py
+++ learn python the hard way.py
@@ -3,3 +3,8 @@ learn python the hard way
 http://learnpythonthehardway.org/book/
 http://www.swaroopch.com/notes/python/
 http://itlab.idcquan.com/linux/manual/python_chinese/index.html
+
+
+# --coding: utf-8 --
+#第一行加入此代码，表示使用utf-8编码
+
