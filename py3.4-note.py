796d92f2e7939989be5562daa717c3fa8c57c9aa
Andor-Z <zyfsta@outlook.com>
2015-09-21 17:26:48 +0800

    add

--- py3.4-note.py
+++ py3.4-note.py
@@ -314,12 +314,31 @@ L = ['Hello', 'World', 18, 'Apple', None]
 
 - 方法一
 把列表生成式的[]改成()
+L = [x *x for x in range(10)]
+L
+
+g = (x*x for x in range(10))
+g
+
+通过使用next()函数获得generator的下一个返回值
 
 
+next(g)
 
+g = (x*x for x in range(10))
+for n in g:
+	print(n)
 
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
 
-
+    
+def triangles(n):
 
 
 
