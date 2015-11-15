3af4e772500fddb9ceb3b5217bf7277455c164e9
Andor_Z <zyfsta@outlook.com>
2015-11-16 00:12:56 +0800

    +

--- Python3_liao/test.py
+++ Python3_liao/test.py
@@ -1,48 +1,7 @@
+# -*- coding:utf-8 -*-
+#__author__ = Andor_ZZ
 
-# -*- coding: utf-8 -*-
-
-#导入re模块
-import re
-
-# 将正则表达式编译成Pattern对象，注意hello前面的r的意思是“原生字符串”
-pattern = re.compile(r'ello')
-
-# 使用re.match匹配文本，获得匹配结果，无法匹配时将返回None
-result1 = re.match(pattern,'hello')
-result2 = re.match(pattern,'helloo CQC!')
-result3 = re.match(pattern,'helo CQC!')
-result4 = re.match(pattern,'hello CQC!')
-
-#如果1匹配成功
-'''
-if result1:
-    # 使用Match获得分组信息
-    print result1.group()
-else:
-    print '1匹配失败！'
-
-
-#如果2匹配成功
-if result2:
-    # 使用Match获得分组信息
-    print result2.group()
-else:
-    print '2匹配失败！'
-
-
-#如果3匹配成功
-if result3:
-    # 使用Match获得分组信息
-    print result3.group()
-else:
-    print '3匹配失败！'
-
-#如果4匹配成功
-if result4:
-    # 使用Match获得分组信息
-    print result4.group()
-else:
-    print '4匹配失败！'
-
-'''
-print(result4)
+week = '星期一星期二星期三星期四星期五星期六星期日'
+date = int(input('please enter date:'))
+weekday = week[(date % 7 -1)*3:(date % 7 -1)*3+3]
+print(weekday) 
