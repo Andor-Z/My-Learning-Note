88c96c30920fe047192cfbbaf72acab1f6fd842b
Andor-Z <zyfsta@outlook.com>
2015-11-17 17:27:59 +0800

    ++

--- Python3_liao/test.py
+++ Python3_liao/test.py
@@ -1,7 +1,5 @@
-# -*- coding:utf-8 -*-
-#__author__ = Andor_ZZ
 
-week = '星期一星期二星期三星期四星期五星期六星期日'
-date = int(input('please enter date:'))
-weekday = week[(date % 7 -1)*3:(date % 7 -1)*3+3]
-print(weekday) 
+
+from datetime import datetime
+now = datetime.now() #获取当前时间
+print(now)
