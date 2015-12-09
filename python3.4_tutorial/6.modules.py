ce3b1f46b4ec0761d6a0e3d82bb2a85c3b3b3bb7
Andor_Z <zyfsta@outlook.com>
2015-12-10 00:35:47 +0800

    ++++

--- python3.4_tutorial/6.modules.py
+++ python3.4_tutorial/6.modules.py
@@ -4,7 +4,7 @@ from numpy.random import randn
 
 
 fig = plt.figure()
-ax1 = fig.add_subplot(221)
+ax1 = fig.add_subplot(2,2,1)
 ax2 = fig.add_subplot(2, 2, 2)
 ax3 = fig.add_subplot(2, 2, 3)
 plt.plot(randn(50).cumsum(), 'k--')
