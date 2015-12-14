efa626691203f03f753354814f7e52dd4589af09
Andor-Z <zyfsta@outlook.com>
2015-12-14 17:27:05 +0800

    +++

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-l = 'abcdef'
-print(l[-1:])
-print(l[:-1])
+import re
+ 
+p = re.compile(r'\d+')
+for m in p.finditer('one1two2three5four4'):
+    print (type(m.group()))
+ 
+### output ###
+# 1 2 3 4
