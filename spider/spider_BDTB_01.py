dba146b0251dd0dd1987e512c54bd8f489102306
Andor_Z <zyfsta@outlook.com>
2015-12-16 23:12:42 +0800

    ++++

--- spider/spider_BDTB_01.py
+++ spider/spider_BDTB_01.py
@@ -4,3 +4,10 @@ import urllib
 import urllib.request as ur 
 import re
 
+# 百度贴吧爬虫类
+class BDTB:
+
+	# 初始化，传入基地址，是否只看楼主的参数
+	def __init__(self, baseUrl, seeLZ):
+		self.baseURL = baseUrl
+		self.seeLZ
