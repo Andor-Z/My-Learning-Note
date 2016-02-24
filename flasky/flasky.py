1764ab1f31236707cf53f31cea69f37cbc65b6f4
Andor_Z <zyfsta@outlook.com>
2016-02-25 00:10:46 +0800

    +++

--- flasky/flasky.py
+++ flasky/flasky.py
@@ -3,6 +3,7 @@ from flask import request
 from flask.ext.script import Manager  
 from flask.ext.bootstrap import Bootstrap 
 from flask.ext.moment import Moment
+from datetime import datetime 
 
 app = Flask(__name__)
 manager = Manager(app)
@@ -12,7 +13,7 @@ moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time = datetime.utcnow())
 
 
 @app.route('/user/<name>')
