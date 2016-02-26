d41eb4c60a237060353459a12452fe441cd7ccc5
Andor-Z <zyfsta@outlook.com>
2016-02-26 17:57:38 +0800

    +++++

--- flasky/flasky.py
+++ flasky/flasky.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session, redirect, url_for, flash
 from flask import request
 from flask.ext.script import Manager  
 from flask.ext.bootstrap import Bootstrap 
@@ -18,12 +18,19 @@ app.config['SECRET_KEY'] = 'flasky key'
 
 
 class NameForm(Form):
-    name = StringField('姓名', validators=[Required])
+    name = StringField('姓名', validators=[Required()])
     submit = SubmitField('提交')
 
-@app.route('/')
+@app.route('/' , methods = ['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time = datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('姓名已经更改！')
+        session['name'] = form.name.data 
+        return redirect(url_for('index'))
+    return render_template('index.html', form = form,name = session.get('name'), current_time = datetime.utcnow())
 
 
 @app.route('/user/<name>')
